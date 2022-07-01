f1cacc4055a8281f47956c675b2e92ad79a13fd1
kellerflint <kflint0068@gmail.com>
2022-07-01 07:25:50 -0700

    added es-lint & prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Anona';
+  fun(hi: any) {
+    console.log(hi);
+  }
 }
